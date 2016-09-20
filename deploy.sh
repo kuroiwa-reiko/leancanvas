7a6dd12cbaaf2a1f4cb9fde2ef3b6e5068b8791b
Kenju Wagatsuma <ken901waga@gmail.com>
2016-09-20 22:53:43 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # git subtree push --prefix build origin gh-pages
 
+# rebuild website
+npm run build
+
 # officially recommended way for deploying to gh-pages
 # https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md#github-pages
 git commit -am "Save local changes"
