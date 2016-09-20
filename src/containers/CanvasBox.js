35b87121f90b77cb70b5518a39747d36f0f6bd57
Kenju Wagatsuma <ken901waga@gmail.com>
2016-09-21 07:56:25 +0900

    Save local changes

--- src/containers/CanvasBox.js
+++ src/containers/CanvasBox.js
@@ -3,13 +3,37 @@
  */
 
 import React, {Component, PropTypes} from 'react';
+import LocalStorageAdapter from '../js/LocalStorageAdapter';
 
 class CanvasBox extends Component {
+    constructor() {
+        super();
+        this.handleOnBlur = this.handleOnBlur.bind(this);
+    }
+
+    componentDidMount() {
+        const id = this.props.id;
+        const text = LocalStorageAdapter.getItem(id);
+        if (text) {
+            document.getElementById(id).innerText = text;
+        }
+    }
+
+    handleOnBlur() {
+        const id = this.props.id;
+        const text = document.getElementById(id).innerText;
+        LocalStorageAdapter.setItem(id, text);
+    }
+
     render() {
         return (
             <div className="canvas-box" id={this.props.id + "_box"}>
                 <h2>{this.props.title}</h2>
-                <div className="canvas-item" id={this.props.id} contentEditable={true}></div>
+                <div className="canvas-item"
+                     id={this.props.id}
+                     contentEditable={true}
+                     onBlur={this.handleOnBlur}
+                ></div>
             </div>
         )
     }
