52cb19579f9ddf982a0b76ca75e2b0136620a3e8
Kenju Wagatsuma <ken901waga@gmail.com>
2016-09-22 22:00:06 +0900

    Create HistoryTable

--- src/containers/Navigation.js
+++ src/containers/Navigation.js
@@ -4,10 +4,10 @@
 
 import React from 'react';
 import NavItems from './NavItems';
+import HistoryTable from './HistoryTable';
 
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
-import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
 
 import {deepOrange500} from 'material-ui/styles/colors';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
@@ -21,21 +21,15 @@ const muiTheme = getMuiTheme({
     },
 });
 
-const dateFromTimestamp = (unix_timestamp) => {
-    const date = new Date(unix_timestamp);
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDay();
-    const hours = date.getHours();
-    const minutes = '0' + date.getMinutes();
-    const seconds = '0' + date.getSeconds();
-    return year + '/' + month + '/' + day + ' ' + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-};
-
 class Navigation extends React.Component {
     state = {
         open: false,
+        selectedRows: [],
         histories: []
+    };
+
+    handleRowSelection = (selectedRows) => {
+        this.setState({selectedRows});
     };
 
     handleOpen = () => {
@@ -47,50 +41,21 @@ class Navigation extends React.Component {
     };
 
     handleApply = () => {
-        // if nothing is selected, simply close
-        // if a new row is selected, update view
-        this.setState({open: false});
+        const selectedRows = this.state.selectedRows;
+        if (selectedRows.length > 0) {
+            // TODO: update canvas
+            const history = this.state.histories[selectedRows[0]];
+            console.log(history);
+        }
+        this.setState({
+            open: false,
+            selectedRows: []
+        });
     };
 
     handleOnShowHistory = (histories) => {
-        this.handleOpen();
         this.setState({histories: histories});
-    };
-
-    generateDialogBody = () => {
-        const histories = this.state.histories;
-        if (histories.length === 0) {
-            return 'No History';
-        }
-
-        const style = {
-            verticalAlign: 'middle'
-        };
-        return (
-            <Table>
-                <TableHeader
-                    displaySelectAll={true}
-                    adjustForCheckbox={false}>
-                    <TableRow className='show-history-dialog-table-row'>
-                        <TableHeaderColumn style={style}>Version</TableHeaderColumn>
-                        <TableHeaderColumn style={style}>Created At</TableHeaderColumn>
-                        <TableHeaderColumn style={style}>Problem</TableHeaderColumn>
-                    </TableRow>
-                </TableHeader>
-                <TableBody
-                    displayRowCheckbox={true}>
-                    {
-                        histories.map((history, index) => (
-                            <TableRow className='show-history-dialog-table-row' key={history.timestamp}>
-                                <TableRowColumn style={style}>{index}</TableRowColumn>
-                                <TableRowColumn style={style}>{dateFromTimestamp(history.timestamp)}</TableRowColumn>
-                                <TableRowColumn style={style}>{history.canvas.problem}</TableRowColumn>
-                            </TableRow>
-                        ))
-                    }
-                </TableBody>
-            </Table>
-        );
+        this.handleOpen();
     };
 
     render() {
@@ -119,7 +84,11 @@ class Navigation extends React.Component {
                         open={this.state.open}
                         onRequestClose={this.handleClose}
                     >
-                        {this.generateDialogBody()}
+                        <HistoryTable
+                            histories={this.state.histories}
+                            selectedRows={this.state.selectedRows}
+                            onRowSelection={this.handleRowSelection}
+                        />
                     </Dialog>
                 </div>
             </MuiThemeProvider>
